5ba31ca340724cc84cf3d1d1241b50a71da79ecc
Aslak Hollund <aslakhol@gmail.com>
2023-08-31 21:28:31 +0200

    create product schema in sanity

---
+++ cms/schemas/product.ts
@@ -0,0 +1,32 @@
+export default {
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  initialValue: {available: true},
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+    },
+    {
+      name: 'available',
+      title: 'Available',
+      type: 'boolean',
+      description: 'Is this product currently available for purchase?',
+    },
+  ],
+  preview: {
+    select: {title: 'name'},
+  },
+}
