dcfa3496f098cece9ab2eda99f895194dc5145f9
Aslak Hollund <aslakhol@gmail.com>
2023-09-04 20:25:11 +0200

    make loading and error states

--- web/src/components/SnackSkeleton.tsx
+++ web/src/components/SnackSkeleton.tsx
@@ -1,3 +1,4 @@
+import { Refrigerator } from "lucide-react";
 import { HowToPay } from "./HowToPay";
 import { SearchAndFilter } from "./SearchAndFilter";
 
@@ -10,8 +11,20 @@ export const SnackSkeleton = ({ isLoading, isError }: Props) => {
       <div className="py-12" />
 
       <HowToPay />
-      {isLoading && <>Loading...</>}
-      {isError && <>Error...</>}
+      <div className="flex flex-1 items-center">
+        {isLoading && (
+          <Refrigerator className="animate-spin text-[#ff5b24]" size={64} />
+        )}
+        {isError && (
+          <div className="flex max-w-sm flex-col gap-4">
+            <p className="text-xl font-semibold">Something has gone wrong.</p>
+
+            <p className="text-lg">
+              You should probably refresh, or go talk to Aslak.
+            </p>
+          </div>
+        )}
+      </div>
     </>
   );
 };
