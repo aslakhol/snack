94ed7b8b7c8ed3dcace4adbf54d0260a2c34a36d
Aslak Hollund <aslakhol@gmail.com>
2024-10-02 21:34:22 +0200

    calculate profit in studio

--- cms/sanity.config.ts
+++ cms/sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import {sanityComputedField} from 'sanity-plugin-computed-field'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: 'mzejrfn3',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(), visionTool(), sanityComputedField()],
 
   schema: {
     types: schemaTypes,
